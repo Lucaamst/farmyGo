6faf034b2dda1c9f279c6966a6935a59a5273555
Luca <luca@amst.ch>
2025-09-17 16:59:28 +0200

    Delete Backend directory

--- Backend/src/Models/models:azienda.js
+++
@@ -1,8 +0,0 @@
-import { DataTypes } from 'sequelize';
-
-export default (sequelize) => {
-  return sequelize.define('Azienda', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    nome: { type: DataTypes.STRING, allowNull: false }
-  });
-};
